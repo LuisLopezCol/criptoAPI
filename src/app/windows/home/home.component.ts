87fc4103e1cf91dc4123278919da993a91920920
luislopezcol <fercholo0730@hotmail.com>
2022-02-10 13:22:01 -0500

    Services to fetch data from previous 15 days added - added table to show the data in the DOM

--- src/app/windows/home/home.component.ts
+++ src/app/windows/home/home.component.ts
@@ -11,24 +11,57 @@ import {  Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   constructor(public coinbaseService: CoinbaseService, public router: Router) { }
   ngOnInit(): void {
-    this.getDate();
+    // this.getDate();
+    this.saveDate(this.firstDay, this.todayDate);
+    this.changeDateFormat(this.todayDate)
+      // this.dailyValue = JSON.parse(localStorage.getItem("dates")!);
+      // console.log(this.dailyValue);
+      
+
   }
+  bitcoinCurrent = [];
+  bitcoinHistoric: any = []; //Final array with all objects fetched from API
   //Find current date
   // todate: any = new Date();
-  todayDate: any = new Date();
-  defStartDate: any = new Date();
-  endDate: any = new Date();
+  test: any;
+  todayDate = new Date();
+  firstDay: any = new Date();
+  lastDay: any = new Date();
   startDate: any = new Date();
-  getDate() {
-    this.defStartDate.setDate(this.todayDate.getDate() - 15);
-    console.log(this.todayDate);
-    console.log(this.defStartDate);
-    this.endDate = this.todayDate.getFullYear()+'-'+(this.todayDate.getMonth()+1)+'-'+this.todayDate.getDate();
-    this.startDate = this.defStartDate.getFullYear()+'-'+(this.defStartDate.getMonth()+1)+'-'+this.defStartDate.getDate();
-    console.log(this.endDate);
-    console.log(this.startDate);
+  dateStart: any;
+  dateEnd: any;
+  changeDateFormat(date: Date){
+    return this.test = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
   }
-  
+
+  getDates() {
+    this.firstDay.setDate(this.todayDate.getDate() - 15);
+    this.dateStart = this.changeDateFormat(this.firstDay);
+    this.dateEnd = this.changeDateFormat(this.lastDay);
+  }
+  //Store the data in the LS in order to have it available when the network crashes
+  saveDate(start: any, end: any) {
+    let historicData: any = {};                                                           //Storing the historic data
+    localStorage.removeItem("historic");                                                  //Clean the LS to fill it again only if we could connect to DB
+    for (let i = 0; i < 15; i++) { 
+      let indexDate = this.changeDateFormat(end);   
+      this.coinbaseService.getPrevious(indexDate).subscribe((res) => {  
+        historicData = res;   
+        historicData.data.date = indexDate;                                //Adding to the res the "date"
+        if(localStorage.getItem("historic") == null){                                      //
+          this.bitcoinHistoric = [];                                                       //Declaring an empty array since no data is in the LS
+          this.bitcoinHistoric.push(historicData);                                         //Pushing the fetch object into the array
+          localStorage.setItem("historic", JSON.stringify(this.bitcoinHistoric));          //Saving the array in  LS
+        }else{
+          this.bitcoinHistoric = JSON.parse(localStorage.getItem("historic")!);            //Getting array from LS to add new object fetched
+          this.bitcoinHistoric.push(historicData);                                         //Pushing object to array
+          end.setDate(end.getDate() - 1 );                                                  //For each loop fetch data for previous date
+          localStorage.setItem("historic", JSON.stringify(this.bitcoinHistoric))};         //Saving the array in  LS
+          console.log("test",JSON.parse(localStorage.getItem("historic")!));
+        })
+        end.setDate(end.getDate() - 1 );                                                  //For each loop fetch data for previous date
+    }
+  };
   viewLoadList: boolean = true;
   
   getCurrent(){
@@ -38,25 +71,14 @@ export class HomeComponent implements OnInit {
       console.log(error);
     })
   }
-    getPrevious(){
-    this.coinbaseService.getPrevious("2022-02-01").subscribe((previousValue) => {  
-      console.log(previousValue);
-      this.saveDate(previousValue)
-    }, error =>{
-      console.log(error);
-    })
-  }
-  //Store the data in the LS in order to have it available when the network crashes
-   totalDates: any;
-    saveDate(date: any) {
-    if(localStorage.getItem("dates") == null){
-      this.totalDates = [];
-      this.totalDates.push(date);
-      localStorage.setItem("dates", JSON.stringify(this.totalDates));
-    }else{
-      this.totalDates = JSON.parse(localStorage.getItem("dates")!);
-      this.totalDates.push(date);
-      localStorage.setItem("dates", JSON.stringify(this.totalDates))};
-    }
-
+  // arrayOfValues: any = {};
+  //   getPrevious(date: any){
+  //   this.coinbaseService.getPrevious(date).subscribe((res) => {  
+  //     console.log(res);
+  //     this.arrayOfValues
+  //   }, error =>{
+  //     console.log(error);
+  //   })
+  // }
+  
 }
