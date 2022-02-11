ece5f543b459a5634fc9ea1cd92aef1ef13ca36b
luislopezcol <fercholo0730@hotmail.com>
2022-02-10 22:27:16 -0500

    all functionalities are completed, next => clean the code and testing

--- src/app/services/coinbase.service.ts
+++ src/app/services/coinbase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { HttpClient, HttpParams} from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,23 +7,28 @@ import { Observable } from 'rxjs';
 })
 export class CoinbaseService {
   //Links to fetch the data
-  urlTRM = "https://www.datos.gov.co/resource/ceyp-9c7c.json?$limit=10000";
+  urlAPICOP = "https://www.datos.gov.co/resource/ceyp-9c7c.json?$limit=10000";
+  urlAPIEUR = "https://api.exchangerate.host";
   urlCurrentValue = 'https://api.coinbase.com/v2/prices/BTC-USD/buy'; 
   urlPreviousValues = 'https://api.coinbase.com/v2/prices/BTC-USD/spot?date=';
 
   constructor(private http: HttpClient) { }
   
   //Get TRM to convert USD to COP
-  public getTRM(){
-      return this.http.get(this.urlTRM);
+  public getEUR(date:any): Observable<any>{
+      return this.http.get(`${this.urlAPIEUR}/${date}`);
+  }
+  //Get TRM to convert USD to COP
+  public getCOP(): Observable<any>{
+      return this.http.get(this.urlAPICOP);
   }
   //Get current value
   public getCurrent(): Observable<any> { 
-    return this.http.get(this.urlCurrentValue);
+    return this.http.get(this.urlAPIEUR);
   }
 
   //Get historics values - concatenate at the end each date (yyyy-mm-dd)
-  public getPrevious(date: any){
+  public getPrevious(date: any): Observable<any>{
      return this.http.get(`${this.urlPreviousValues}${date}`);
   }
 }
--- src/app/windows/home/home.component.ts
+++ src/app/windows/home/home.component.ts
@@ -10,21 +10,17 @@ import { DetailComponent} from '../detail/detail.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(public coinbaseService: CoinbaseService, public router: Router) { }
-    //  @ViewChild("startDay") startDay!: ElementRef;
+  constructor(public coinbaseService: CoinbaseService, public router: Router, private renderer2:Renderer2) { }
 
-     
     ngOnInit(): void {
       this.getCurrent();
       this.saveDate(true, this.firstDay, this.firstDay);
       
       this.changeDateFormat(this.todayDate);
       console.log("this is today", this.dateStart);
-      //  let startDay = this.startDay.nativeElement;
-      //  startDay.value = this.dateStart;
   }
   
-  //----------- Get cuurent Bitcoin Value
+  //----------- Get Curent Bitcoin Value
   fetchedTime  = new Date();
   @ViewChild("restart") restart!: ElementRef;
   getCurrent(){
@@ -57,7 +53,7 @@ export class HomeComponent implements OnInit {
   changeDateFormat(date: Date){
     return this.test = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
   }
-
+  
   saveDate(newQuery: boolean, start: any, end: any) {
     let totalDays: number; //Loop
     
@@ -72,10 +68,10 @@ export class HomeComponent implements OnInit {
       start = new Date(start);
       end = new Date(end);
           console.log(start);
-    console.log("this is my test",end);
+          console.log("this is my test",end);
 
     }
-
+    
     let historicData: any = {};                                                           //Storing the historic data
     localStorage.removeItem("historic");                                                  //Clean the LS to fill it again only if we could connect to DB
     for (let i = 0; i < totalDays; i++) { 
@@ -95,9 +91,57 @@ export class HomeComponent implements OnInit {
           console.log("test",JSON.parse(localStorage.getItem("historic")!));
         })
         end.setDate(end.getDate() - 1 );                                                  //For each loop fetch data for previous date
-    }
-  };
-
-
-  
+      }
+    };
+    
+    
+    
+  //----------- Get Other Currencies
+  @ViewChild('toastDetail') toastDetail!: ElementRef;
+  @ViewChild('backdrop') backdrop!: ElementRef;
+  showToast () {
+    const toastDetail = this.toastDetail.nativeElement;
+    const backdrop = this.backdrop.nativeElement;
+    this.renderer2.addClass(toastDetail,"show-toast");
+    this.renderer2.removeClass(toastDetail,"hide-toast");
+    this.renderer2.addClass(backdrop,"show-toast");
+  this.renderer2.removeClass(backdrop,"hide-toast");
+  setTimeout(() => {
+    this.hideToast();
+  }, 30000);
+  }
+  hideToast () {
+    const toastDetail = this.toastDetail.nativeElement;
+    const backdrop = this.backdrop.nativeElement;
+    this.renderer2.addClass(toastDetail,"hide-toast");
+    this.renderer2.removeClass(toastDetail,"show-toast");
+    this.renderer2.addClass(backdrop,"hide-toast");
+    this.renderer2.removeClass(backdrop,"show-toast");
+  }
+  openToast(date: any, amount: any){
+    this.showToast();
+  }
+  ExchRateEUR: any;
+  ExchRateCOP: any;
+  ExchRateUSD: any;
+  getTRM(date: any, USD: any){
+    this.coinbaseService.getEUR(date).subscribe((res) => {  
+      this.ExchRateUSD = USD;
+      this.ExchRateEUR =  (this.ExchRateUSD/res.rates.USD);
+      this.ExchRateCOP = (this.ExchRateUSD/(res.rates.USD/res.rates.COP));
+      console.log(date, "cop", this.ExchRateCOP);
+      console.log(date, "eur", this.ExchRateEUR);
+      console.log(date, "cusd", this.ExchRateUSD);
+      
+      console.log(this.ExchRateEUR);
+      
+      setTimeout(() => {
+        this.getCurrent();
+        let restart = this.restart.nativeElement;
+        restart.click();
+      }, 60500);
+    }, error =>{
+      console.log(error);
+    })
+  }
 }
